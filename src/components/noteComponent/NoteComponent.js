d3da049993dc73178e0166c0c5248948eb80e954
Templars <antoine.ratat@gmail.com>
2021-10-21 14:35:53 +0800

    render notes

--- src/components/noteComponent/NoteComponent.js
+++ src/components/noteComponent/NoteComponent.js
@@ -3,13 +3,12 @@ import React from 'react'
 export default function NoteComponent(props) {
 	const {
 		note,
-		handleToggleNote,
-		isLoadingToggleNote,
-		handleDeleteNote,
-		isLoadingDelete,
+		// handleToggleNote,
+		// isLoadingToggleNote,
+		// handleDeleteNote,
+		// isLoadingDelete,
 	} = props
 
-	console.log(note)
 	return (
 		<div>
 			{note.note_id} {note.note_title} {note.note_content}{' '}
