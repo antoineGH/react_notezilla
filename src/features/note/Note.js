32c7133becae046b00f20321855d1707fd248d77
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-07 20:21:37 +0800

    export steps joyride utils file

--- src/features/note/Note.js
+++ src/features/note/Note.js
@@ -11,6 +11,7 @@ import {
 import { selectSearch } from '../search/searchSlice'
 import { updateNote } from '../note/NoteSlice'
 import Joyride, { STATUS, ACTIONS } from 'react-joyride'
+import { steps } from './utils/steps'
 import NoteListComponent from '../../components/noteListComponent/NoteListComponent'
 import AddNoteComponent from '../../components/addNoteComponent/AddNoteComponent'
 import LastNoteComponent from '../../components/lastNoteComponent/LastNoteComponent'
@@ -28,34 +29,6 @@ export default function Note(props) {
   const noteIdUpdated = useSelector(selectNoteIdUpdated)
   const [sort, setSort] = useState(false)
   const [sortBy, setSortBy] = useState('Date')
-
-  var steps = [
-    {
-      target: '#edit-note',
-      content: 'Want to edit an existing note? Click here!',
-      disableBeacon: true,
-    },
-    {
-      target: '.autosave-note',
-      content: 'Did I save my..? Say no more! Notes are automatically saved.',
-      disableBeacon: true,
-    },
-    {
-      target: '#btn-cross',
-      content: 'Want to get rid of your note? Click here!',
-      disableBeacon: true,
-    },
-    {
-      target: '.container-addnote',
-      content: 'Feeling inspired? Write a new note!',
-      disableBeacon: true,
-    },
-    {
-      target: '.container-scratchpad',
-      content: 'Start writing your ideas on the scratchpad!',
-      disableBeacon: true,
-    },
-  ]
 
   useEffect(() => {
     dispatch(loadNotes())
